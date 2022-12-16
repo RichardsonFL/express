9b73bba8a95e3559b687e9b70391e600f0992b42
Richardson Florencio <richardson.florencio5@gmail.com>
2022-12-15 22:48:38 -0300

    ultimo test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 //const bodyParser = require("body-parser")    // also native node module
 
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT ||  5000
 const userRoutes = require("./routes/userRoutes")
 
 const app = express();  //instance os Express
