ab7a161b8745095470f4c8e39c80750783518b69
Richardson Florencio <richardson.florencio5@gmail.com>
2022-12-24 05:01:49 -0300

    ...

--- routes/consultRoute.js
+++ routes/consultRoute.js
@@ -4,16 +4,19 @@ const consultRoute = (app) => {
     app.route("/states/:uf?")
     .get((req, res) => {
         if(!req.params.uf){
-            const siglas = estados.map(item => item.sigla)
-            res.status(200).send(siglas)
-        }
-
-        try{
-            const UF =req.params.uf
-            const { nome, cidades } = estados.find(uf => uf.sigla === UF.toUpperCase())
-            res.status(200).send({nome, cidades})
-        } catch(err){
-            res.status(400).send({stc: "400", UF: "Not Found"})
+            const states = estados.map(({sigla, nome}) => {
+                return {sigla, nome}
+                }
+            )
+            res.status(200).send(states)
+        } else{
+            try{
+                const UF = req.params.uf
+                const { nome, cidades } = estados.find(uf => uf.sigla === UF.toUpperCase())
+                res.status(200).send({nome, cidades})
+            } catch(err){
+                res.status(400).send({stc: "400", UF: "Not Found"})
+            }
         }
     })
 }
